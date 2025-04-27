11fc6b2c297a1ee2aa00b4fe482599bbab1acffe
Aman K <amankumarmatta0@gmail.com>
2025-04-27 17:09:25 +0530

    Grid and Trays Generation Fixed

--- Coffee Jam/Assets/Scripts/TrayController.cs
+++ Coffee Jam/Assets/Scripts/TrayController.cs
@@ -1,18 +1,14 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-
 public class TrayController : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     public Vector2Int gridPosition;
     private bool isDragging;
     private Vector3 offset;
-
     private void Start()
     {
-        // Ensure correct initial rotation
         transform.rotation = Quaternion.Euler(-90f, 0f, 0f);
     }
-
     public void OnDrag(PointerEventData eventData)
     {
         if (!isDragging)
@@ -21,22 +17,18 @@ public class TrayController : MonoBehaviour, IDragHandler, IEndDragHandler
             offset = transform.position - Camera.main.ScreenToWorldPoint(
                 new Vector3(eventData.position.x, eventData.position.y, 10f));
         }
-        
         Vector3 cursorPoint = new Vector3(eventData.position.x, eventData.position.y, 10f);
         Vector3 cursorWorldPos = Camera.main.ScreenToWorldPoint(cursorPoint);
         transform.position = cursorWorldPos + offset;
     }
-
     public void OnEndDrag(PointerEventData eventData)
     {
         isDragging = false;
         SnapToGrid();
     }
-
     private void SnapToGrid()
     {
         Vector2Int newGridPos = GridManager.Instance.WorldToGridPosition(transform.position);
-        
         if (GridManager.Instance.IsGridPositionValid(newGridPos))
         {
             gridPosition = newGridPos;
@@ -44,20 +36,14 @@ public class TrayController : MonoBehaviour, IDragHandler, IEndDragHandler
         }
         else
         {
-            // Return to original position
             transform.position = GridManager.Instance.GridToWorldPosition(gridPosition);
         }
-        
-        // Maintain correct rotation
         transform.rotation = Quaternion.Euler(-90f, 0f, 0f);
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Handle collisions with other trays
         if (collision.gameObject.CompareTag("Tray") && !isDragging)
         {
-            // Add slight push effect
             Vector2 pushDirection = (collision.transform.position - transform.position).normalized;
             collision.gameObject.transform.Translate(pushDirection * 0.1f);
         }
